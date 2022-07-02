f77bada26dd83ed5cf4caf4af44042b719ea3908
BurakCD <50319976+BurakCD@users.noreply.github.com>
2022-07-02 16:46:48 +0300

    Initial commit

---
+++ app/src/main/java/com/burak/fragment_kullanimi/MainActivity.java
@@ -0,0 +1,22 @@
+package com.burak.fragment_kullanimi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    private void FragmentStart(Fragment fragment){
+
+    }
+}
