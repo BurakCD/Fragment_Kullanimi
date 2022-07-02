dbadd04161c5fbc0ca02933371a6b30afba4dbd4
BurakCD <50319976+BurakCD@users.noreply.github.com>
2022-07-02 18:47:13 +0300

    fragment manager finish

--- app/src/main/java/com/burak/fragment_kullanimi/MainActivity.java
+++ app/src/main/java/com/burak/fragment_kullanimi/MainActivity.java
@@ -1,7 +1,10 @@
 package com.burak.fragment_kullanimi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -12,11 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        AppCompatButton button1 = findViewById(R.id.fragmentFirst);
+        AppCompatButton button2 = findViewById(R.id.fragmentLast);
 
-
+        button1.setOnClickListener((e)-> {
+            FragmentStart(new Fragment1());
+        });
+        button2.setOnClickListener((e)-> {
+            FragmentStart(new Fragment2());
+        });
     }
 
     private void FragmentStart(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager(); // fragment işlemlerini yapan metot
+        FragmentTransaction transaction = fragmentManager.beginTransaction(); // işlemi başlatan yapı
+
+        transaction.add(R.id.FrameLayout,fragment); //transactiona fragmentleri ekleyeceği bir frame layout ve fragment eklenir
+        transaction.commit(); // işlemi başlatma
 
     }
 }
